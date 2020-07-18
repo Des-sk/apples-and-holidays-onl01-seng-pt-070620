aaaaf2cd97f897dee40bc6ec800620c2bc0d8e8a
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-18 23:50:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,6 +34,7 @@ def all_winter_holiday_supplies(holiday_hash)
 holiday_hash [:winter]. map do |key, value|
   value
 end.flatten
+end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
