b02b1f5c91b36d583dda0d5af0198f10c07740d2
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-18 22:57:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,5 +1,6 @@
 
-def second_supply_for_fourth_of_july[:summer][:fourth_of_july][1]
+def second_supply_for_fourth_of_july(holiday_supplies)
+  holiday_supplies[:summer][:fourth_of_july][1]
 
 second_supply_for_fourth_of_july
 end
