98687ee2562b89f1d9166ba96cb50dc6ffc77fac
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-19 00:35:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,6 +44,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holiday|
+    puts "#{season.to_s.capitalize}:"
+    holiday.each do |event, supplies|
+      puts "#{event.to_s.split(" ""
 
 end
 
