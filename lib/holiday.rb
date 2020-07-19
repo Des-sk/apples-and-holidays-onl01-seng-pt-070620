779a97754f72609505daa6381e06c0670a170918
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-19 01:19:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |event, supplies|
-      puts "  #{event.to_s.split(" ").map{|x|x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+      puts "  #{event.to_s.(" ").map{|x|x.capitalize}.join(" ")}: #{supplies.join(", ")}"
     end
  end
 end
